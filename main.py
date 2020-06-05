b60838d5162b83013aa4c2787e4f49f92bc9c3cf
jgabriellacerda <gabriel.lacerda@engenharia.ufjf.br>
2020-06-05 16:50:26 -0300

    Adicionado inimigo e imagem de fundo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Inicializa o pygame
 pygame.init()
@@ -6,21 +7,36 @@ pygame.init()
 # Cria a tela
 screen = pygame.display.set_mode((800, 600))
 
+# Imagem de fundo
+background = pygame.image.load('background.png')
+
 # Titulo e Icone
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('alien.png')
+icon = pygame.image.load('icone.png')
 pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load('spaceship.png')
 playerX = 370
-playerY = 480
+playerY = 64*8
 playerX_change = 0
-player_speed = 0.5
+player_speed = 8
+
+# Inimigo
+inimigoImg = pygame.image.load('alien.png')
+inimigoX = random.randint(0, 800)
+inimigoY = 0
+inimigo_speed = 6
+inimigoX_change = inimigo_speed
+inimigoY_change = 64
 
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def inimigo(x, y):
+    screen.blit(inimigoImg, (x, y))
 
 
 # Loop do jogo
@@ -28,7 +44,9 @@ running = True
 while running:
 
     # RGB - Red, Green, Blue
-    screen.fill((200, 200, 255))
+    screen.fill((200, 200, 230))
+    # Imagem de fundo
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,7 +62,29 @@ while running:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                 playerX_change = 0
 
+    # Atualiza posicao do player
     playerX += playerX_change
+
+    # Garante que o player nao ultrapasse o limite da tela
+    if playerX < 0:
+        playerX = 0
+    if playerX > 800 - 64:
+        playerX = 800 - 64
+
+    # Atualiza posicao do inimigo
+    inimigoX += inimigoX_change
+
+    # Garante que o player nao ultrapasse o limite da tela
+    if inimigoX < 0:
+        inimigoX = 0
+        inimigoX_change = -inimigoX_change
+        inimigoY += inimigoY_change
+    if inimigoX > 800 - 64:
+        inimigoX = 800 - 64
+        inimigoX_change = -inimigoX_change
+        inimigoY += inimigoY_change
+
     player(playerX, playerY)
+    inimigo(inimigoX, inimigoY)
 
     pygame.display.update()
