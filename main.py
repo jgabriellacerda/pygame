36cc354b464c353bf5e166a46617e6047598c2a5
jgabriellacerda <gabriel.lacerda@engenharia.ufjf.br>
2020-06-05 16:04:22 -0300

    Adicionado movimento ao player usando as teclas

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import pygame
 
-#Inicializa o pygame
+# Inicializa o pygame
 pygame.init()
 
-#Cria a tela
-screen = pygame.display.set_mode((800,600))
+# Cria a tela
+screen = pygame.display.set_mode((800, 600))
 
-#Titulo e Icone
+# Titulo e Icone
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('alien.png')
 pygame.display.set_icon(icon)
 
-#Player
+# Player
 playerImg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+player_speed = 0.5
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
-#Loop do jogo
+# Loop do jogo
 running = True
 while running:
 
-    #RGB - Red, Green, Blue
+    # RGB - Red, Green, Blue
     screen.fill((200, 200, 255))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # Se uma tecla foi pressionada
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                playerX_change = player_speed
+            if event.key == pygame.K_LEFT:
+                playerX_change = -player_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    player(playerX, playerY)
 
     pygame.display.update()
-
